2afa68124bd721eae9aaa3991ba1815869df2adf
karanimaina <karani.maina2010@gmail.com>
2023-01-06 16:05:26 +0300

    modify supplier component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {SupplierComponent} from "./supplier/supplier.component";
+import {CustomerComponent} from "./customer/customer.component";
 
 const routes: Routes = [
   {path:'dashboard',component:DashboardComponent},
-  {path:'supplier',component:SupplierComponent}
+  {path:'supplier',component:SupplierComponent},
+  {path:'customer',component:CustomerComponent}
 ];
 
 @NgModule({
