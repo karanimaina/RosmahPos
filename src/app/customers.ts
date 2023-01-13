993c14a4b4f9ff676c561bbe7711eb509933189c
karanimaina <karani.maina2010@gmail.com>
2023-01-13 09:49:15 +0300

    add customerName

--- src/app/customers.ts
+++ src/app/customers.ts
@@ -1,2 +1,10 @@
 export class Customers {
+  customerName:string
+  phoneNumber: string
+  debt :number
+  constructor(customerName: string, phoneNumber: string, debt: number) {
+    this.customerName = customerName;
+    this.phoneNumber = phoneNumber;
+    this.debt = debt;
+  }
 }
